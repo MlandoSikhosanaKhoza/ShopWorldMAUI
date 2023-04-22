96fa455cf6176542620a4c5acba87223dcc7eb20
mlando2.0 <mlando2.0@outlook.com>
2023-04-22 13:42:06 +0200

    App Resourses added

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
 
 namespace ShopWorld.MAUI;
 
@@ -13,7 +14,7 @@ public static class MauiProgram
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+			}).UseMauiCommunityToolkit();
 
 #if DEBUG
 		builder.Logging.AddDebug();
