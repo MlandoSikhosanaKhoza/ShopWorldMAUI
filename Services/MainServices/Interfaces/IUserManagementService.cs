c106d253accc257b760b655a67a06a41ba6a2a6c
mlando2.0 <mlando2.0@outlook.com>
2023-04-26 03:17:41 +0200

    Service for UserMangement added

---
+++ Services/MainServices/Interfaces/IUserManagementService.cs
@@ -0,0 +1,14 @@
+﻿using ShopWorld.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopWorld.MAUI.Services
+{
+    public interface IUserManagementService
+    {
+        Task<LoginResult> LoginAsUser(string Mobile);
+    }
+}
